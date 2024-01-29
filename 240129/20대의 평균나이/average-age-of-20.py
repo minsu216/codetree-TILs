3f550646f6408b26cbd8428a863bf06a7141ece8
Minsu <96571949+minsu216@users.noreply.github.com>
2024-01-29 17:39:20 +0900

    Title: 20대의 평균나이, Time: 149ms, Memory: 24MB, Status: Passed - CodeTree

--- 240129/20대의 평균나이/average-age-of-20.py
+++ 240129/20대의 평균나이/average-age-of-20.py
@@ -1,13 +1,17 @@
-i=1
-result=0
-lst=[]
+i = 1
+result = 0
+lst = []
+
 while True:
-    n=int(input())
-    if n>=30 and len(lst)==0 and n<20:
+    try:
+        n = int(input())
+        if n < 20 or n >= 30: # 20이하 30이상이면 종료
+            break
+        else:
+            lst.append(n)
+            i += 1
+    except EOFError:
         break
-    if n>=30 and len(lst)!=0:
-        print("%.2f"%(sum(lst)/(i-1)))
-        break
-    else:
-        lst.append(n)
-        i+=1
+
+if lst:  # lst가 비어있지 않은 경우만 평균 계산
+    print("%.2f" % (sum(lst) / (i - 1)))
