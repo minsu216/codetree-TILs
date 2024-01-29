f0e64d6d689b5af64be2e850a2f5616fc8ed45a2
Minsu <96571949+minsu216@users.noreply.github.com>
2024-01-29 17:26:35 +0900

    Title: 20대의 평균나이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240129/20대의 평균나이/average-age-of-20.py
+++ 240129/20대의 평균나이/average-age-of-20.py
@@ -1,17 +1,9 @@
-# result=0
-# for i in range(1,7):
-#     n=int(input())
-#     if n>=30:
-#         print("%.2f"%(result/(i-1)))
-#     else:
-#         result+=n
-
-i=0
+i=1
 result=0
 while True:
     n=int(input())
     if n>=30:
-        print("%.2f"%(result/i))
+        print("%.2f"%(result/(i-1)))
         break
     else:
         result+=n
